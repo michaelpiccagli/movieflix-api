2f426dc1025e8e28bcd1e60b6fa6a749872c624f
Michael Piccagli <michaelpiccagli@outlook.com>
2026-02-05 13:08:40 -0300

    adicionado swagger do método GET

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument) )
 app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
     orderBy: {
@@ -109,7 +111,7 @@ app.get("/movies/:genreName", async (req, res) => {
         },
       },
     });
-    
+
     res.status(200).send(moviesFilteredByGenreName);
   } catch (error) {
     return res.status(500).send({ message: "Falha ao filtrar filmes por gênero" });
